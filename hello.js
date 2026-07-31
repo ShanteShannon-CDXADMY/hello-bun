8982e2c27b8cbd3e954b6ce35921c4af8d8bc034
Shante Shannon <shantesheree90@icloud.com>
2026-07-30 22:08:25 -0400

    print the date

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ let course = "Level 2";
 let message = "Hello from " + course + "!";
 console.log(message);
 console.log("This JavaScript runs on MY machine.");
+console.log("Today is July 30, 2026");
