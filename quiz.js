a66ac8003eebeb40c4993a3f051a99124c99c60e
Shante Shannon <shantesheree90@icloud.com>
2026-07-31 22:05:27 -0400

    add question 2

--- quiz.js
+++ quiz.js
@@ -3,4 +3,11 @@ let q1 = prompt("What planet is known as the Red Planet?");
 if (q1 === "Mars"){
 console.log("Correct!")}
 else {
+console.log("Wrong Answer!")}; 
+
+let q2 =prompt("How many continents are there on Earth?");
+
+if (q2 === "7"){
+console.log("Correct!")}
+else {
 console.log("Wrong Answer!")};
