9113cb2bf7461c35d96de4fa8e1e9e9449c7fee5
Shante Shannon <shantesheree90@icloud.com>
2026-07-31 21:21:38 -0400

    chat script with three questions

---
+++ chat.js
@@ -0,0 +1,6 @@
+let name = prompt("What is your name?");
+console.log("Nice to meet you, " + name + "!");
+let food = prompt("What is your favorite food?");
+console.log("I also love " + food + "!");
+let luckyNumber = prompt("What is your favorite number?");
+console.log(luckyNumber + " is a lucky number!");
